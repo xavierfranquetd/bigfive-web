6d08e15799722129c33c2a3ab8e3f861efd5f418
xavierfranquetd <xavierfranquetd@gmail.com>
2026-03-18 21:54:02 -0400

    Remove social media sharing buttons from ShareBar
    Removed Facebook and Twitter sharing options from the ShareBar component.

--- web/src/components/share-bar.tsx
+++ web/src/components/share-bar.tsx
@@ -1,7 +1,6 @@
 'use client';
-
 import { Button, Tooltip } from '@nextui-org/react';
-import { CopyIcon, FacebookIcon, PDFIcon, TwitterIcon } from './icons';
+import { CopyIcon, PDFIcon } from './icons';
 import { Link as NextUiLink } from '@nextui-org/link';
 import { Report } from '@/actions/index';
 import { useCopyToClipboard } from '@/hooks/use-copy-to-clipboard';
@@ -12,39 +11,9 @@ interface ShareBarProps {
 
 export default function ShareBar({ report }: ShareBarProps) {
   const [_, copy] = useCopyToClipboard();
-
   const handleCopy = (text: string) => async () => await copy(text);
-
   return (
     <>
-      <Tooltip color='secondary' content='Share on facebook'>
-        <Button
-          isIconOnly
-          aria-label='Share on facebook'
-          radius='full'
-          size='md'
-          variant='light'
-          as={NextUiLink}
-          isExternal
-          href={`https://www.facebook.com/sharer/sharer.php?u=https://bigfive-test.com/result/${report.id}`}
-        >
-          <FacebookIcon size={48} />
-        </Button>
-      </Tooltip>
-      <Tooltip color='secondary' content='Share on X'>
-        <Button
-          isIconOnly
-          aria-label='Share on X'
-          radius='full'
-          size='md'
-          variant='light'
-          target='_blank'
-          as={NextUiLink}
-          href={`https://twitter.com/intent/tweet?text=See my personality traits!&url=https://bigfive-test.com/result/${report.id}`}
-        >
-          <TwitterIcon size={42} />
-        </Button>
-      </Tooltip>
       <Tooltip color='secondary' content='Download PDF'>
         <Button
           isIconOnly
